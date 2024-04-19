9725a52ecdc0515f89d23a0ddd1870babdff15c2
Pramod Adhav <Pramodadhav111@gmail.com>
2024-04-19 16:13:01 +0530

    Updated Backend

--- QUERY.sql
+++ QUERY.sql
@@ -1,62 +1,245 @@
 CREATE DATABASE ELECTRONICS;
-USE ELECTRONICS;
-SELECT * FROM PRODUCT ORDER BY CAST(SUBSTRING(P_ID, 2,LEN(P_ID) - 1 ) AS INT);
-SELECT * FROM PURCHASE ORDER BY CAST(SUBSTRING(USER_ID, 2,LEN(USER_ID) - 1 ) AS INT);
-SELECT * FROM Users ORDER BY CAST(SUBSTRING(USER_ID, 2,LEN(USER_ID) - 1 ) AS INT);
+	USE ELECTRONICS;
+	SELECT * FROM PRODUCT ORDER BY CAST(SUBSTRING(P_ID, 2,LEN(P_ID) - 1 ) AS INT);
+	SELECT * FROM PURCHASE ORDER BY CAST(SUBSTRING(P_ID, 2,LEN(P_ID) - 1 ) AS INT);
+	SELECT * FROM USERS ORDER BY CAST(SUBSTRING(USER_ID, 2,LEN(USER_ID) - 1 ) AS INT);
 
-----Q1----
-SELECT CATEGORY, AVG(RATING) AS Avg_Rating
-    FROM Product
-    GROUP BY CATEGORY;
+---Q1: NUMBER OF PURCHASES ACC TO YEARS---
+SELECT YEAR, COUNT(*) AS Purchase_Count
+FROM PURCHASE
+GROUP BY YEAR ORDER BY YEAR;
 
-----Q2----
-SELECT RATING
-    FROM Product;
+---Q2: NUMBER OF PURCHASES BASED ON THE BRAND---
+SELECT BRAND, COUNT(*) AS Purchase_Count
+FROM PRODUCT
+GROUP BY BRAND;
 
-----Q3----
-SELECT TIMESTAMP, COUNT(*) AS Purchase_Count
-    FROM Purchase
-    GROUP BY TIMESTAMP;
+---Q3: NUMBER OF PURCHASES BASED ON THE ITEM_ID---
+SELECT ITEM_ID, COUNT(*) AS Purchase_Count
+FROM PRODUCT
+GROUP BY ITEM_ID
+ORDER BY Purchase_Count DESC;
 
-----Q4----
-SELECT CATEGORY, RATING
-    FROM Product;
+---Q4: NUMBER OF PURCHASES OF MALE AND FEMALE---
+SELECT USER_ATTR, COUNT(*) AS Purchase_Count
+FROM USERS
+GROUP BY USER_ATTR;
 
-----Q5----
-SELECT MONTH, COUNT(*) AS Purchase_Count
-    FROM Purchase
-    GROUP BY MONTH ORDER BY MONTH;
 
-----Q6----
-SELECT BRAND, RATING
-    FROM Product;
+---Q5: NO OF PRODUCTS WITH DIFFERENT RATINGS---
+SELECT RATING, COUNT(*) AS Purchase_Count
+FROM PRODUCT
+GROUP BY RATING ORDER BY RATING;
+-----------------------------------------------
+---Q6: NO OF PURCHASES BASED ON MODEL ATTRIBUTE---
+SELECT MODEL_ATTR, COUNT(*) AS Purchase_Count
+FROM PRODUCT
+GROUP BY MODEL_ATTR
+ORDER BY Purchase_Count DESC;
 
-----Q7----
-SELECT P.RATING, U.USER_ATTR
-    FROM Product P
-    INNER JOIN Purchase PU ON P.P_ID = PU.P_ID
-    INNER JOIN Users U ON PU.USER_ID = U.USER_ID;
+----------------------------------------------
+--- Q7: NUMBER OF PURCHASES BASED ON ITEM_ID---
+SELECT ITEM_ID, COUNT(*) AS Purchase_Count
+FROM PRODUCT
+GROUP BY ITEM_ID;
 
-----Q8----
-SELECT RATING, CATEGORY, BRAND
-    FROM Product;
+----------------------------------------------
+--- Q8: NUMBER OF PURCHASES MADE IN EACH MONTH BASED ON PARTICULAR YEAR---
+SELECT YEAR, MONTH, COUNT(*) AS Purchase_Count
+FROM PURCHASE
+WHERE YEAR = 2000 
+GROUP BY YEAR, MONTH ORDER BY MONTH;
 
-----Q9----
-SELECT P.RATING, PU.TIMESTAMP
-    FROM Product P
-    INNER JOIN Purchase PU ON P.P_ID = PU.P_ID;
+SELECT YEAR, MONTH, COUNT(*) AS Purchase_Count
+FROM PURCHASE
+WHERE YEAR = 2001 
+GROUP BY YEAR, MONTH ORDER BY MONTH;
 
-----Q10----
-SELECT CATEGORY, COUNT(*) AS Category_Count
-    FROM Product
-    GROUP BY CATEGORY;
+SELECT YEAR, MONTH, COUNT(*) AS Purchase_Count
+FROM PURCHASE
+WHERE YEAR = 2002  
+GROUP BY YEAR, MONTH ORDER BY MONTH;
 
-----Q11----
-SELECT TIMESTAMP, AVG(RATING) AS Avg_Rating
-    FROM Purchase PU
-    INNER JOIN Product P ON PU.P_ID = P.P_ID
-    GROUP BY TIMESTAMP;
+SELECT YEAR, MONTH, COUNT(*) AS Purchase_Count
+FROM PURCHASE
+WHERE YEAR = 2003 
+GROUP BY YEAR, MONTH ORDER BY MONTH;
 
-----Q12----
-SELECT BRAND, RATING
-    FROM Product;
+SELECT YEAR, MONTH, COUNT(*) AS Purchase_Count
+FROM PURCHASE
+WHERE YEAR = 2004  
+GROUP BY YEAR, MONTH ORDER BY MONTH;
+
+SELECT YEAR, MONTH, COUNT(*) AS Purchase_Count
+FROM PURCHASE
+WHERE YEAR = 2005
+GROUP BY YEAR, MONTH ORDER BY MONTH;
+
+SELECT YEAR, MONTH, COUNT(*) AS Purchase_Count
+FROM PURCHASE
+WHERE YEAR = 2006 
+GROUP BY YEAR, MONTH ORDER BY MONTH;
+----------------------------------------------
+---Q9: AVGERAGE RATING BASED ON BRAND---
+SELECT BRAND, AVG(RATING) AS Avg_Rating
+FROM PRODUCT
+GROUP BY BRAND;
+
+--- Q10: Identify the top 3 brands with the highest average rating---
+SELECT *
+FROM (
+    SELECT BRAND, AVG(RATING) AS Avg_Rating,
+           ROW_NUMBER() OVER (ORDER BY AVG(RATING) DESC) AS rank
+    FROM PRODUCT
+    GROUP BY BRAND
+) AS ranked
+WHERE rank <= 3;
+
+
+------------------------------------------------------------
+---Q11: IDENTIFYING THE SALES PER YEAR IN QUARTERS 
+SELECT YEAR, 
+       CASE
+           WHEN MONTH IN (1, 2, 3) THEN 'Q1'
+           WHEN MONTH IN (4, 5, 6) THEN 'Q2'
+           WHEN MONTH IN (7, 8, 9) THEN 'Q3'
+           ELSE 'Q4'
+       END AS Quarter,
+       COUNT(*) AS Purchase_Count
+FROM PURCHASE
+WHERE YEAR = 1999
+GROUP BY YEAR, 
+         CASE
+           WHEN MONTH IN (1, 2, 3) THEN 'Q1'
+           WHEN MONTH IN (4, 5, 6) THEN 'Q2'
+           WHEN MONTH IN (7, 8, 9) THEN 'Q3'
+           ELSE 'Q4'
+         END;
+
+SELECT YEAR, 
+       CASE
+           WHEN MONTH IN (1, 2, 3) THEN 'Q1'
+           WHEN MONTH IN (4, 5, 6) THEN 'Q2'
+           WHEN MONTH IN (7, 8, 9) THEN 'Q3'
+           ELSE 'Q4'
+       END AS Quarter,
+       COUNT(*) AS Purchase_Count
+FROM PURCHASE
+WHERE YEAR = 2000
+GROUP BY YEAR, 
+         CASE
+           WHEN MONTH IN (1, 2, 3) THEN 'Q1'
+           WHEN MONTH IN (4, 5, 6) THEN 'Q2'
+           WHEN MONTH IN (7, 8, 9) THEN 'Q3'
+           ELSE 'Q4'
+         END;
+
+SELECT YEAR, 
+       CASE
+           WHEN MONTH IN (1, 2, 3) THEN 'Q1'
+           WHEN MONTH IN (4, 5, 6) THEN 'Q2'
+           WHEN MONTH IN (7, 8, 9) THEN 'Q3'
+           ELSE 'Q4'
+       END AS Quarter,
+       COUNT(*) AS Purchase_Count
+FROM PURCHASE
+WHERE YEAR = 2001
+GROUP BY YEAR, 
+         CASE
+           WHEN MONTH IN (1, 2, 3) THEN 'Q1'
+           WHEN MONTH IN (4, 5, 6) THEN 'Q2'
+           WHEN MONTH IN (7, 8, 9) THEN 'Q3'
+           ELSE 'Q4'
+         END;
+
+SELECT YEAR, 
+       CASE
+           WHEN MONTH IN (1, 2, 3) THEN 'Q1'
+           WHEN MONTH IN (4, 5, 6) THEN 'Q2'
+           WHEN MONTH IN (7, 8, 9) THEN 'Q3'
+           ELSE 'Q4'
+       END AS Quarter,
+       COUNT(*) AS Purchase_Count
+FROM PURCHASE
+WHERE YEAR = 2002
+GROUP BY YEAR, 
+         CASE
+           WHEN MONTH IN (1, 2, 3) THEN 'Q1'
+           WHEN MONTH IN (4, 5, 6) THEN 'Q2'
+           WHEN MONTH IN (7, 8, 9) THEN 'Q3'
+           ELSE 'Q4'
+         END;
+
+SELECT YEAR, 
+       CASE
+           WHEN MONTH IN (1, 2, 3) THEN 'Q1'
+           WHEN MONTH IN (4, 5, 6) THEN 'Q2'
+           WHEN MONTH IN (7, 8, 9) THEN 'Q3'
+           ELSE 'Q4'
+       END AS Quarter,
+       COUNT(*) AS Purchase_Count
+FROM PURCHASE
+WHERE YEAR = 2003
+GROUP BY YEAR, 
+         CASE
+           WHEN MONTH IN (1, 2, 3) THEN 'Q1'
+           WHEN MONTH IN (4, 5, 6) THEN 'Q2'
+           WHEN MONTH IN (7, 8, 9) THEN 'Q3'
+           ELSE 'Q4'
+         END;
+
+SELECT YEAR, 
+       CASE
+           WHEN MONTH IN (1, 2, 3) THEN 'Q1'
+           WHEN MONTH IN (4, 5, 6) THEN 'Q2'
+           WHEN MONTH IN (7, 8, 9) THEN 'Q3'
+           ELSE 'Q4'
+       END AS Quarter,
+       COUNT(*) AS Purchase_Count
+FROM PURCHASE
+WHERE YEAR = 2004
+GROUP BY YEAR, 
+         CASE
+           WHEN MONTH IN (1, 2, 3) THEN 'Q1'
+           WHEN MONTH IN (4, 5, 6) THEN 'Q2'
+           WHEN MONTH IN (7, 8, 9) THEN 'Q3'
+           ELSE 'Q4'
+         END;
+
+SELECT YEAR, 
+       CASE
+           WHEN MONTH IN (1, 2, 3) THEN 'Q1'
+           WHEN MONTH IN (4, 5, 6) THEN 'Q2'
+           WHEN MONTH IN (7, 8, 9) THEN 'Q3'
+           ELSE 'Q4'
+       END AS Quarter,
+       COUNT(*) AS Purchase_Count
+FROM PURCHASE
+WHERE YEAR = 2005
+GROUP BY YEAR, 
+         CASE
+           WHEN MONTH IN (1, 2, 3) THEN 'Q1'
+           WHEN MONTH IN (4, 5, 6) THEN 'Q2'
+           WHEN MONTH IN (7, 8, 9) THEN 'Q3'
+           ELSE 'Q4'
+         END;
+
+SELECT YEAR, 
+       CASE
+           WHEN MONTH IN (1, 2, 3) THEN 'Q1'
+           WHEN MONTH IN (4, 5, 6) THEN 'Q2'
+           WHEN MONTH IN (7, 8, 9) THEN 'Q3'
+           ELSE 'Q4'
+       END AS Quarter,
+       COUNT(*) AS Purchase_Count
+FROM PURCHASE
+WHERE YEAR = 2006
+GROUP BY YEAR, 
+         CASE
+           WHEN MONTH IN (1, 2, 3) THEN 'Q1'
+           WHEN MONTH IN (4, 5, 6) THEN 'Q2'
+           WHEN MONTH IN (7, 8, 9) THEN 'Q3'
+           ELSE 'Q4'
+         END;
+
+------------------------------------------------
--- Visualization.py
+++ Visualization.py
@@ -1,80 +1,232 @@
-from flask import Flask, render_template
 import pyodbc
 import pandas as pd
 import matplotlib.pyplot as plt
-from io import BytesIO
-import base64
+from flask import Flask, render_template
+import seaborn as sns
 
 app = Flask(__name__)
 
-
-
-# Establish a connection to the database
-cnxn = pyodbc.connect(r'Driver=SQL Server;Server=.\SQLEXPRESS;Database=ELECTRONICS;Trusted_Connection=yes;')
-
 @app.route('/')
 def index():
-    # Query to fetch data for visualization
-    query1 = '''
-        SELECT gender, COUNT(*) as count
-        FROM user_table
-        GROUP BY gender
-    '''
+    cnxn = pyodbc.connect(r'Driver=SQL Server;Server=.\SQLEXPRESS;Database=ELECTRONICS;Trusted_Connection=yes;')
+    cursor = cnxn.cursor()
+
+    #############  QUERY-1:Number of purchases according to years ###############
+    query1 = """
+                    SELECT YEAR, COUNT(*) AS Purchase_Count
+                    FROM PURCHASE
+                    GROUP BY YEAR ORDER BY YEAR;
+                    """
     df1 = pd.read_sql_query(query1, cnxn)
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='YEAR', y='Purchase_Count', data=df1)
+    plt.xlabel('Year')
+    plt.ylabel('Purchase Count')
+    plt.title('Number of Purchases According to Years')
+    Viz1 = 'static/Purchases_Per_Year.png'
+    plt.savefig(Viz1)
+    plt.close()
 
-    # Query to fetch data for visualization
-    query2 = '''
-        SELECT age_group, COUNT(*) as count
-        FROM user_table
-        GROUP BY age_group
-    '''
-    df2 = pd.read_sql_query(query2, cnxn)
+    #############  QUERY-2: Number of purchases based on brand ###############
+    query2 = """
+                    SELECT BRAND, COUNT(*) AS Purchase_Count 
+                    FROM PRODUCT GROUP BY BRAND;
+                    """
+    df2 = pd.read_sql(query2, cnxn)
+    plt.figure(figsize=(9 , 5))
+    sns.barplot(x='BRAND', y='Purchase_Count', data=df2)
+    plt.xlabel('Brand')
+    plt.ylabel('Purchase Count')
+    plt.title('Number of Purchases Based on Brand')
+    plt.xticks(rotation=45)
+    Viz2 = 'static/Brand_Purchases.png'
+    plt.savefig(Viz2)
+    plt.close()
 
-    # Query to fetch data for visualization
-    query3 = '''
-        SELECT social_media_platform, COUNT(*) as count
-        FROM user_social_media
-        GROUP BY social_media_platform
-    '''
-    df3 = pd.read_sql_query(query3, cnxn)
+    #############  QUERY-3: Number of purchases based on item_id ###############
+    query3 = """
+                    SELECT ITEM_ID, COUNT(*) AS Purchase_Count 
+                    FROM PRODUCT GROUP BY ITEM_ID 
+                    ORDER BY Purchase_Count DESC;
+                    """
+    df3 = pd.read_sql(query3, cnxn)
+    plt.figure(figsize=(10, 8))
+    sns.barplot(x='ITEM_ID', y='Purchase_Count', data=df3)
+    plt.xlabel('Item ID')
+    plt.ylabel('Purchase Count')
+    plt.title('Number of Purchases Based on Item ID')
+    plt.xticks(rotation=45)
+    Viz3 = 'static/Purchases_Per_ItemID.png'
+    plt.savefig(Viz3)
+    plt.close()
 
-    # Query to fetch data for visualization
-    query4 = '''
-        SELECT time_of_day, COUNT(*) as count
-        FROM user_activity
-        GROUP BY time_of_day
-    '''
-    df4 = pd.read_sql_query(query4, cnxn)
+    #############  QUERY-4: Number of purchases based on User Atrribute ###############
+    query4 = """
+                SELECT USER_ATTR, COUNT(*) AS Purchase_Count 
+                FROM USERS GROUP BY USER_ATTR;"""
+    df4 = pd.read_sql(query4, cnxn)
+    plt.figure(figsize=(7, 7))
+    plt.pie(df4['Purchase_Count'], labels=df4['USER_ATTR'], autopct='%1.1f%%', startangle=140)
+    plt.title('Number of Purchases Based on User Attribute')
+    Viz4 = 'static/Purchases_Per_Attributes.png'
+    plt.savefig(Viz4)
+    plt.close()
 
-    # Query to fetch data for visualization
-    query5 = '''
-        SELECT u.USER_ATTR, COUNT(*) as user_count
-        FROM user_table u
-        JOIN connection_table c ON u.USER_ID = c.USER_ID
-        GROUP BY u.USER_ATTR
-    '''
-    df5 = pd.read_sql_query(query5, cnxn)
+    #############  QUERY-5: Number of products with different ratings ###############
+    query5 = """
+                SELECT RATING, COUNT(*) AS Purchase_Count 
+                FROM PRODUCT GROUP BY RATING
+                ORDER BY RATING;
+                """
+    df5 = pd.read_sql(query5, cnxn)
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='RATING', y='Purchase_Count', data=df5)
+    plt.xlabel('Rating')
+    plt.ylabel('Number of Products')
+    plt.title('Number of Products with Different Ratings')
+    Viz5 = 'static/Product_Ratings.png'
+    plt.savefig(Viz5)
+    plt.close()
 
-    # Drop unnecessary columns after the join
-    # df5.drop(columns=['column_to_drop_from_joined_table'], inplace=True)  # Replace 'column_to_drop_from_joined_table' with the actual column name
+    #############  QUERY-6: Number of purchases based on Model Attributes ###############
+    query6 = """
+                SELECT MODEL_ATTR, COUNT(*) AS Purchase_Count 
+                FROM PRODUCT GROUP BY MODEL_ATTR 
+                ORDER BY Purchase_Count DESC;
+                """
+    df6 = pd.read_sql(query6, cnxn)
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='MODEL_ATTR', y='Purchase_Count', data=df6)
+    plt.xlabel('Model Attribute')
+    plt.ylabel('Purchase Count')
+    plt.title('Number of Purchases Based on Model Attribute')
+    plt.xticks(rotation=45)
+    Viz6 = 'static/Products_Based_on_Model_Attribute.png'
+    plt.savefig(Viz6)
+    plt.close()
 
-    # Plotting
-    plt.figure(figsize=(8, 5))
-    plt.bar(df5['USER_ATTR'], df5['user_count'])
-    plt.xlabel('User Attribute')
-    plt.ylabel('User Count')
-    plt.title('User Attribute Distribution')
+    #############  QUERY-7: NUMBER OF PURCHASES MADE IN EACH MONTH BASED ON PARTICULAR YEAR ###############
+    years = range(1999, 2007)  # Range of years to visualize
+
+    fig, axes = plt.subplots(4, 2, figsize=(8,8))
+
+    for i, year in enumerate(years):
+        query7 = f"""SELECT MONTH, COUNT(*) AS Purchase_Count FROM PURCHASE 
+                    WHERE YEAR = {year} GROUP BY YEAR, MONTH 
+                    ORDER BY MONTH;"""
+        df7 = pd.read_sql(query7, cnxn)
+        
+        # Check if the dataframe is empty
+        if not df7.empty:
+            row = i // 2
+            col = i % 2
+            ax = axes[row, col]
+            ax.bar(df7['MONTH'], df7['Purchase_Count'])
+            ax.set_title(f'Year {year}')
+            ax.set_xlabel('Month')
+            ax.set_ylabel('Purchase Count')
+        else:
+            # If dataframe is empty, set a placeholder title
+            row = i // 2
+            col = i % 2
+            ax = axes[row, col]
+            ax.set_title(f'Year {year} (No Data)')
+            ax.axis('off')  # Turn off axis for empty plots
+            
+    plt.tight_layout()
+    Viz7 = 'static/Monthly_Purchases_By_Year.png'
+    plt.savefig(Viz7)
+    plt.close()
+
+    #############  QUERY-8: AVERAGE RATING BASED ON BRAND ###############
+    query8 = """
+                SELECT BRAND, AVG(RATING) AS Avg_Rating 
+                FROM PRODUCT GROUP BY BRAND;
+                """
+    df8 = pd.read_sql(query8, cnxn)
+    plt.figure(figsize=(9, 6))
+    sns.barplot(x='BRAND', y='Avg_Rating', data=df8)
+    plt.xlabel('Brand')
+    plt.ylabel('Average Rating')
+    plt.title('Average Rating Based on Brand')
+    plt.xticks(rotation=45)
+    Viz8 = 'static/AverageRatings_Based_On_Brand.png'
+    plt.savefig(Viz8)
+    plt.close()
+
+    #############  QUERY-9: Identify the top 3 brands with the highest average rating ###############
+    query9 = """
+            SELECT *
+            FROM (
+                SELECT BRAND, AVG(RATING) AS Avg_Rating,
+                    ROW_NUMBER() OVER (ORDER BY AVG(RATING) DESC) AS rank
+                FROM PRODUCT
+                GROUP BY BRAND
+            ) AS ranked
+            WHERE rank <= 3;
+            """
+    df9 = pd.read_sql(query9, cnxn)
+    plt.figure(figsize=(10, 6))
+    plt.bar(df9['BRAND'], df9['Avg_Rating'], color='skyblue')
+    plt.xlabel('Brand')
+    plt.ylabel('Average Rating')
+    plt.title('Top 3 Brands with Highest Average Rating')
     plt.xticks(rotation=45)
     plt.tight_layout()
+    Viz9 = 'static/Top_3_Brands_With_Highest_Avg_Rating.png'
+    plt.savefig(Viz9)
+    plt.close()
 
-    # Convert plot to PNG image
-    img = BytesIO()
-    plt.savefig(img, format='png')
-    img.seek(0)
-    plot_url = base64.b64encode(img.getvalue()).decode()
+    #############  QUERY-10: IDENTIFYING THE SALES PER YEAR IN QUARTERS ###############
+    years = range(1999, 2007)  # Range of years to visualize
 
-    # Pass the plot URL to the HTML template
-    return render_template('index.html', plot_url=plot_url)
+    fig, axes = plt.subplots(8, 1, figsize=(9, 16))  # 8 rows, 1 column for vertical arrangement
+
+    for i, year in enumerate(years):
+        ax = axes[i]  # Select the axis for the current year
+        
+        # Query to select data for each year
+        query10 = f"""
+                SELECT CASE
+                        WHEN MONTH IN (1, 2, 3) THEN 'Q1'
+                        WHEN MONTH IN (4, 5, 6) THEN 'Q2'
+                        WHEN MONTH IN (7, 8, 9) THEN 'Q3'
+                        ELSE 'Q4'
+                    END AS Quarter,
+                    COUNT(*) AS Purchase_Count
+                FROM PURCHASE
+                WHERE YEAR = {year}
+                GROUP BY YEAR, 
+                        CASE
+                        WHEN MONTH IN (1, 2, 3) THEN 'Q1'
+                        WHEN MONTH IN (4, 5, 6) THEN 'Q2'
+                        WHEN MONTH IN (7, 8, 9) THEN 'Q3'
+                        ELSE 'Q4'
+                        END;
+                """
+        df10 = pd.read_sql(query10, cnxn)
+        ax.bar(df10['Quarter'], df10['Purchase_Count'], color='skyblue')
+        ax.set_title(f'Year {year}')
+        ax.set_xlabel('Quarter')
+        ax.set_ylabel('Purchase Count')
+
+    plt.tight_layout()
+    Viz10 = 'static/Purchase_Count_By_Quarter.png'
+    plt.savefig(Viz10)
+    plt.close()
+
+
+    return render_template('index.html', Viz1='static/Purchases_Per_Year.png',
+                           Viz2='static/Brand_Purchases.png',
+                           Viz3='static/Purchases_Per_ItemID.png',
+                           Viz4='static/Purchases_Per_Attributes.png',
+                           Viz5='static/Product_Ratings.png',
+                           Viz6='static/Products_Based_on_Model_Attribute.png',
+                           Viz7='static/Monthly_Purchases_By_Year.png',
+                           Viz8='static/AverageRatings_Based_On_Brand.png',
+                           Viz9='static/Top_3_Brands_With_Highest_Avg_Rating.png',
+                           Viz10='static/Purchase_Count_By_Quarter.png',
+                           )
 
 if __name__ == '__main__':
     app.run(debug=True)
